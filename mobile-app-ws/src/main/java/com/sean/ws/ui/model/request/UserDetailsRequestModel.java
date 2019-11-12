88a35da5576fad72b8b78683df6c4207f4cd6ecf
SeanLim <happylim99@gmail.com>
2019-11-12 11:44:46 +0800

    implement create user request model

---
+++ mobile-app-ws/src/main/java/com/sean/ws/ui/model/request/UserDetailsRequestModel.java
@@ -0,0 +1,36 @@
+package com.sean.ws.ui.model.request;
+
+// handle incoming json request into java object
+public class UserDetailsRequestModel {
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
