5afb8bbe8228281ef46deb4b009eaf1f69a6ea09
SeanLim <happylim99@gmail.com>
2019-11-13 15:54:30 +0800

    implement authorization filter

--- mobile-app-ws/src/main/java/com/sean/ws/security/WebSecurity.java
+++ mobile-app-ws/src/main/java/com/sean/ws/security/WebSecurity.java
@@ -27,7 +27,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests()
 		.antMatchers(HttpMethod.POST, "/users").permitAll()
-        .anyRequest().authenticated().and().addFilter(getAuthenticationFilter());
+        .anyRequest().authenticated().and().addFilter(getAuthenticationFilter())
+        .addFilter(new AuthorizationFilter(authenticationManager()));
 	}
 	
 	@Override
