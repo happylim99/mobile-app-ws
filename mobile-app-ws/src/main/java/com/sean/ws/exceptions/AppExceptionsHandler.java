733911f2e1c0f241561b20b45bb9345d8bc86d80
SeanLim <happylim99@gmail.com>
2019-11-20 17:25:45 +0800

    return custom error object message

--- mobile-app-ws/src/main/java/com/sean/ws/exceptions/AppExceptionsHandler.java
+++ mobile-app-ws/src/main/java/com/sean/ws/exceptions/AppExceptionsHandler.java
@@ -1,5 +1,7 @@
 package com.sean.ws.exceptions;
 
+import java.util.Date;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,13 +9,17 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import com.sean.ws.ui.model.response.ErrorMessage;
+
 @ControllerAdvice
 public class AppExceptionsHandler {
 	
 	@ExceptionHandler(value = {UserServiceException.class})
 	public ResponseEntity<Object> handleUserServiceException(UserServiceException ex, WebRequest request)
 	{
-		return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+		ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+		//return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+		return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 }
