f5b16066afbb0b4a74cf3916a45a7687cf7e70a5
SeanLim <happylim99@gmail.com>
2019-11-25 11:46:13 +0800

    create AddressDto

--- mobile-app-ws/src/main/java/com/sean/ws/shared/dto/UserDto.java
+++ mobile-app-ws/src/main/java/com/sean/ws/shared/dto/UserDto.java
@@ -1,6 +1,7 @@
 package com.sean.ws.shared.dto;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class UserDto implements Serializable {
 
@@ -15,6 +16,7 @@ public class UserDto implements Serializable {
 	private String encryptedPassword;
 	private String emailVerificationToken;
 	private Boolean emailVerificationStatus = false;
+	private List<AddressDto> addresses;
 
 	public UserDto() {
 		super();
@@ -102,6 +104,14 @@ public class UserDto implements Serializable {
 
 	public void setEmailVerificationStatus(Boolean emailVerificationStatus) {
 		this.emailVerificationStatus = emailVerificationStatus;
+	}
+
+	public List<AddressDto> getAddresses() {
+		return addresses;
+	}
+
+	public void setAddresses(List<AddressDto> addresses) {
+		this.addresses = addresses;
 	}
 
 }
