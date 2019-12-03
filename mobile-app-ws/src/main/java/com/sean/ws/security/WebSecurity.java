ac5c89e3355c2cca69efc1205558e78709b19eab
SeanLim <happylim99@gmail.com>
2019-12-03 16:18:16 +0800

    add aws ses dependency and configure security class

--- mobile-app-ws/src/main/java/com/sean/ws/security/WebSecurity.java
+++ mobile-app-ws/src/main/java/com/sean/ws/security/WebSecurity.java
@@ -29,8 +29,11 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests()
-		.antMatchers(HttpMethod.POST, "/users").permitAll()
-		//.antMatchers(HttpMethod.GET, "/users/hello").permitAll()
+		//.antMatchers(HttpMethod.POST, "/users").permitAll()
+		.antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
+		.permitAll()
+		.antMatchers(HttpMethod.GET, SecurityConstants.EMAIL_VERIFICATION)
+		.permitAll()
         .anyRequest().authenticated()
 		.and().addFilter(getAuthenticationFilter())
         .addFilter(new AuthorizationFilter(authenticationManager()))
